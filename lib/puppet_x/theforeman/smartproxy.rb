197adfd9e177e09d6ed4da4ee6aa886c7db6d208
liamjbennett <lbennett@opentable.com>
2014-11-05 13:58:23 +0000

    fixing the location of settings.yaml for smart_proxy

--- lib/puppet_x/theforeman/smartproxy.rb
+++ lib/puppet_x/theforeman/smartproxy.rb
@@ -9,7 +9,7 @@ module Resources
   class SmartProxy < Resource
 
     def initialize(resource)
-      super(resource, '/etc/foreman_proxy/settings.yaml')
+      super(resource, '/etc/foreman-proxy/settings.yaml')
     end
 
     def create(proxy_hash)
